2f73f9b9df1e1c332fff8c6ef2acd8c770cb105a
MDaryaK <dasha-41@mail.ru>
2023-04-10 00:21:30 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -41,6 +41,33 @@ function handleFormSubmit(evt) {
 
 form.addEventListener('submit', handleFormSubmit);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 //Создаем переменные
 const imagePopup = document.querySelector('.popup_type_photo');
 const imagePopupPhoto = imagePopup.querySelector('.popup__photo');
